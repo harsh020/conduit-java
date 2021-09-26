b0514e28c39cf2f40b8cccdf4b0ecdb9bea085f7
Harsh Soni <devilincarcerated020@yahoo.com>
2021-09-26 18:27:28 +0530

    change base path

--- src/main/java/com/example/conduit/user/UserController.java
+++ src/main/java/com/example/conduit/user/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api")
 public class UserController {
     UserService userService;
     UserObjectConverter converter;
