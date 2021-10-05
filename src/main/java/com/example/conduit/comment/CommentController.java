109d6e9d705aaad8e8b2da43913f56ce5e5c72c4
Harsh Soni <devilincarcerated020@yahoo.com>
2021-10-06 01:07:53 +0530

    Add comment service

--- src/main/java/com/example/conduit/comment/CommentController.java
+++ src/main/java/com/example/conduit/comment/CommentController.java
@@ -1,13 +1,37 @@
 package com.example.conduit.comment;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.conduit.comment.dtos.request.CommentRequest;
+import com.example.conduit.user.models.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class CommentController {
 
-    @PostMapping("/{slug}/{id}")
+    @PostMapping("/articles/{slug}/comments")
+    public String createComment(@AuthenticationPrincipal User user,
+                                @PathVariable String slug,
+                                @RequestBody CommentRequest commentDetail) {
+        return "Create new comment";
+    }
 
+    @GetMapping("/articles/{slug}/comments/{id}")
+    public String getComment(@AuthenticationPrincipal User user,
+                             @PathVariable String slug,
+                             @PathVariable Integer id) {
+        return "Getting single comment";
+    }
+
+    @GetMapping("/articles/{slug}/comments")
+    public String getComments(@AuthenticationPrincipal User user, @PathVariable String slug) {
+        return "Getting all comments for article " + slug;
+    }
+
+    @PutMapping("/articles/{slug}/comments/{id}")
+    public  String updateComment(@AuthenticationPrincipal User user,
+                                 @PathVariable String slug,
+                                 @PathVariable Integer id) {
+        return "Update comment";
+    }
 }
